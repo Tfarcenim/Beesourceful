5c82319d4b103b517b5d2ad25c70749ec815188f
Tfarcenim <44327798+Gimpler@users.noreply.github.com>
2020-02-28 06:28:52 -0500

    interface methods are always public

--- src/main/java/com/tfar/beesourceful/recipe/CentrifugeRecipeType.java
+++ src/main/java/com/tfar/beesourceful/recipe/CentrifugeRecipeType.java
@@ -1,19 +1,8 @@
 package com.tfar.beesourceful.recipe;
 
-import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.item.crafting.IRecipeType;
-import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.registry.Registry;
 
 public class CentrifugeRecipeType {
 
-  public static final IRecipeType<CentrifugeRecipe> CENTRIFUGE = register("beesourceful:centrifuge");
-
-  public static <T extends IRecipe<?>> IRecipeType<T> register(final String name) {
-    return Registry.register(Registry.RECIPE_TYPE, new ResourceLocation(name), new IRecipeType<T>() {
-      public String toString() {
-        return name;
-      }
-    });
-  }
+  public static final IRecipeType<CentrifugeRecipe> CENTRIFUGE = IRecipeType.register("beesourceful:centrifuge");
 }
